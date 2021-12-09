59ec1ce93a09087e8387df0d3019753b1aaef117
Sissel Uglem <89508910+SisselUglem@users.noreply.github.com>
2021-12-09 14:25:18 +0100

    added js

--- script.js
+++ script.js
@@ -1,102 +1,83 @@
 // BANK ACCOUNT
 
-//ASSIGMENT 2-------------------------------------------------------
+// OBJEKT
+const account = {
+  accountName: "Account one",
+  balance: 100,
+
+  getBalance: function () {
+    alert(`Your balance on ${this.accountName} is ${this.balance} dollars`);
+  },
+
+  deposit: function () {
+    let amount = parseFloat(prompt("How much would you like to deposit?"));
+    this.balance = this.balance + amount;
+
+    if (isNaN(amount) || amount === " " || amount <= 0) {
+      alert(`You can only enter numbers and amounts over 0 dollars`);
+      this.deposit();
+    } else {
+      alert(`Your deposit has been successfully completed.`);
+    }
+  },
+
+  withdrawal: function () {
+    let amount = parseFloat(prompt("How much would you like to withdrawal?"));
+    this.balance = this.balance - amount;
+
+    if (isNaN(amount) || amount === " " || amount <= 0) {
+      alert(`You can only enter numbers and amounts over 0 dollars`);
+      this.deposit();
+    } else {
+      alert(`Your withdrawal has been successfully completed.`);
+    }
+  },
+
+  getAccountName: function () {
+    alert(`Current account name is ${this.accountName}`);
+  },
+
+  accountError: function () {
+    alert(`You must enter a number from 1 to 5`);
+  },
+
+  exitAccount: function () {
+    window.opener = self;
+    window.close();
+  },
+}
+
 
 // STARTER FUNCTION
 function atm() {
   let choice = parseInt(
     prompt(
-      "Welcome to the ATM Bank! Choose an option: [1. See Balance] [2. Make desposit] [3. Make a withdrawal] [4. Get account name] [5. Exit]"
-    )
-  );
-  //If choise is 1(See balance) -> siplay balance
+      `Welcome to the ATM Bank! Choose an option: 
+      1. See Balance 
+      2. Make desposit 
+      3. Make a withdrawal 
+      4. Get account name 
+      5. Exit`));
+
+  //User makes a choice from 1-5. each number calls a different function from the main object.
   if (choice === 1) {
-    getBalance();
+    account.getBalance();
     atm();
   } else if (choice === 2) {
-    deposit();
+    account.deposit();
     atm();
   } else if (choice === 3) {
-    withdrawal();
+    account.withdrawal();
     atm();
   } else if (choice === 4) {
-    getAccountName();
+    account.getAccountName();
     atm();
   } else if (choice === 5) {
-    exitAccount();
+    account.exitAccount();
   } else {
-    accountError();
-  }
-}
-
-// OBJEKT
-//const account = {
-//accountName: "Account one",
-//balance: 100,
-//getBalance: function () {
-//alert(`Your balance on ${this.accountName} is ${this.balance} dollars`);},
-//deposit: function () {
-//parseFloat(prompt("How much would you like to deposit?"));},
-//withdrawal: function () {
-//parseFloat(prompt("How much would you like to withdrawal?"));},
-//getAccountName: function () {},
-//accountError: function () {},
-//exitAccount: function () {},
-//};
-
-//created separate varables, did not get the variables from the objekt to work in the main/start function with this.Name
-const accountName = "Account one";
-let balance = 100;
-
-//FUNCTIONS
-//created separate functions, did not get the functions from the objekt to work in the main/start function with this.Name
-function getBalance() {
-  alert(`Your balance on ${accountName} is ${balance} dollars.`);
-}
-
-function deposit() {
-  let amount = parseFloat(prompt("How much would you like to deposit?"));
-  balance = balance + amount;
-
-  if (isNaN(amount)) {
-    alert(`You must enter a number`);
-  } else if (amount === 0) {
-    alert(`You can't deposit an amount of 0`);
-  } else {
-    alert(`Your deposit has been successfully completed.`);
-  }
-}
-
-function withdrawal() {
-  let amount = parseFloat(prompt("How much would you like to withdrawal?"));
-  balance = balance - amount;
-
-  if (isNaN(amount)) {
-    alert(`You must enter a number`);
-  } else if (amount === 0) {
-    alert(`You can't withdrawal an amount of 0`);
-  } else {
-    alert(`Your deposit has been successfully completed.`);
-  }
-}
-
-function getAccountName() {
-  alert(`Current account name is ${accountName}`);
-}
-
-function accountError(choice) {
-  if (isNaN(choice)) {
-    alert(`You must enter a number from 1 to 5`);
-    atm();
-  } else if ((choice === 0, 6, 7, 8, 9)) {
-    alert(`You must enter a number from 1 to 5`);
+    account.accountError();
     atm();
   }
-}
-
-function exitAccount() {
-  window.opener = self;
-  window.close();
 }
 
 atm();
